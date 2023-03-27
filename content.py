0d9c7c89e6edecc8c21c0286ab5e86aa0545893b
Yonas Marie~ <yonasgebre08@gmail.com>
2023-03-28 00:14:57 +0300

    accepted comments

--- content.py
+++ content.py
@@ -1,14 +1,26 @@
 
 count = 0
 
-
-def abc(x, y):
-    return x * y
+# incremets the value of the global variable count
 
 
 def increment_count():
     global count
     count += 1
+
+# multiplies two numbers
+
+
+def product(x, y):
+    return x * y
+
+
+def calculateArea(radius):
+    return 3.14 * radius ** 2
+
+
+def divide_numbers(a, b):
+    return a / b
 
 
 increment_count()
