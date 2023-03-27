bde1fd48dbafa941ec17010a14c0d9ae2a0ca7a6
Tseganesh Yifru <t5691yifru@gmail.com>
2023-03-28 00:27:17 +0300

    modifying snippet_1 based on the comments

--- snippet_1.py
+++ snippet_1.py
@@ -14,12 +14,20 @@ class CircleMeasurement:
     def CircleArea(radius):
         return 3.14 * radius ** 2
 
+
 class Oprerations:
     def __init__(self, a, b):
         self.a = a
-        self.b = b
-    
+        self.b = b    
     # dividing two numbers
-    def divide_numbers(a, b):
-        return a / b
-    # we can add other operations here
+    def divide_numbers(self):
+        return self.a / self.b
+    # adding two numbers
+    def multily_numbers(self):
+        return self.a * self.b
+    # subtracting two numbers
+    def subtract_numbers(self):
+        return self.a - self.b
+    # adding two numbers
+    def add_numbers(self):
+        return self.a + self.b
