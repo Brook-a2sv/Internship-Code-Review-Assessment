2375093ea7472da377d2f565609e6895b9b974cb
Yonas Marie~ <yonasgebre08@gmail.com>
2023-03-28 00:27:02 +0300

    clean up content.py

--- content.py
+++ content.py
@@ -1,28 +1,26 @@
 count = 0
 
-# incremets the value of the global variable count
-
 
 def increment_count():
+    # incremets the value of the global variable count
     global count
     count += 1
 
-# multiplies two numbers
-
 
 def product(x, y):
+    # multiplies two numbers
     return x * y
 
 
-def calculateArea(radius):
-    return 3.14 * radius ** 2
-
-
 def divide_numbers(a, b):
+    # divides two numbers
     return a / b
+
+
+def calculateArea(radius):
+    # calculates the area of a circle with a given radius
+    return 3.14 * radius ** 2
 
 
 increment_count()
 print(count)
-
-
