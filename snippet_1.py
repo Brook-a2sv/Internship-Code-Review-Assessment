8c7e9fd1e4d159d571c57accdc08129ad627e645
Tseganesh Yifru <t5691yifru@gmail.com>
2023-03-27 23:15:07 +0300

    refactor snippet_1.py

--- snippet_1.py
+++ snippet_1.py
@@ -1,5 +1,22 @@
-def calculateArea(radius):
-    return 3.14 * radius ** 2
 
-def divide_numbers(a, b):
-    return a / b
+import math
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def Parameter(self):
+        return 2 * math.pi * self.radius
+    
+    def calculateArea(radius):
+        return 3.14 * radius ** 2
+
+class Oprerations:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    # dividing two numbers
+    def divide_numbers(a, b):
+        return a / b
+    # we can add other operations here
